484c465e886bed7fb5e9dd5b6712405d65620b8a
Ilya Lelkov <ilya.lelkov@mail.ru>
2024-07-02 16:18:33 +1000

    create authorization page

--- frontend/src/Service.jsx
+++ frontend/src/Service.jsx
@@ -3,6 +3,7 @@ import { Test } from './components/test/test.jsx'
 import { ErrorPage } from './pages/error-page/errorPage.jsx'
 import { ERROR } from './constans'
 import { Registration } from './pages/registretion/registration.jsx'
+import { Authorization } from './pages/authorization/authorization.jsx'
 import './App.css'
 
 function Service() {
@@ -10,7 +11,8 @@ function Service() {
 		<>
 			<Routes>
 				<Route path="/" element={<Test />} />
-				<Route path="/login" element={<Registration />} />
+				<Route path="/login" element={<Authorization />} />
+				<Route path="/register" element={<Registration />} />
 				<Route path="*" element={<ErrorPage error={ERROR.PAGE_NOT_EXIST} />} />
 			</Routes>
 		</>
---
+++ frontend/src/pages/authorization/authorization.jsx
@@ -0,0 +1,57 @@
+import { Input } from '../../components/input/input.jsx'
+import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useState } from 'react'
+import { authFormSchema } from './authorizationSchema/authorizationSchema.js'
+import style from './authorization.module.css'
+import styleInput from '../registretion/styles/input/input.module.css'
+
+export const Authorization = () => {
+	const [serverError, setServerError] = useState(null)
+
+	const { register, handleSubmit, formState: { errors } } = useForm({
+		defaultValues: {
+			login: '',
+			password: ''
+		},
+		resolver: yupResolver(authFormSchema)
+	})
+
+	const submit = (data) => {
+		console.log(data)
+	}
+
+	const formError = errors?.login?.message || errors?.password?.message || errors?.checkPassword?.message
+
+	return (
+		<>
+			<div className={style.registration_container}>
+				<h2>Авторизация</h2>
+				<form className={style.registration_form} onSubmit={handleSubmit(submit)}>
+					<div className={style.input_container}>
+						<Input {...register('login')} text="Введите имя..." type="text" styleClass={styleInput}
+									 autoComplete="username" />
+						<Input {...register('password')} text="Введите пароль..." type="password" styleClass={styleInput}
+									 autoComplete="new-password" />
+						{/* TODO: вынести блок ошибок в отдельный компонент */}
+						<div className={style.error_container}>
+							<p>{errors.login && errors.login.message}</p>
+							<p>{errors.password && errors.password.message}</p>
+							<p>{errors.checkPassword && errors.checkPassword.message}</p>
+						</div>
+					</div>
+					<div className={style.button_container}>
+						<button disabled={!!formError} type="submit">Зарегистрироваться</button>
+						<Link to="/">
+							<button className={style.button_main}>Главная страница</button>
+						</Link>
+						<Link to="/register">
+							<button className={style.button_registration}>Регистрация</button>
+						</Link>
+					</div>
+				</form>
+			</div>
+		</>
+	)
+}
