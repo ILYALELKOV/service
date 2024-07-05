873a032b769123c41f190fea9387a3ff8b62467a
Ilya Lelkov <ilya.lelkov@mail.ru>
2024-07-05 11:40:44 +1000

    add png user, calendar, login and small changes header

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -10,7 +10,6 @@ export const Header = () => {
 				<button className={style.logo_image}></button>
 			</Link>
 
-
 			<form action="#" className={style.input_container}>
 				<Input
 					styleClass={mainInputStyle}
@@ -20,13 +19,31 @@ export const Header = () => {
 				<button className={style.btn_search}></button>
 			</form>
 
+			<div className={style.date_container}>
+				<div className={style.calendar}></div>
+				<p>{new Date().toLocaleString('ru', {
+					year: 'numeric',
+					day: 'numeric',
+					month: 'long'
+				})}</p>
+				<p>{new Date().toLocaleString('ru', {
+					weekday: 'long'
+				})}</p>
+			</div>
+
 
 			<div className={style.user_container}>
-				<h3>User?</h3>
-				<Link to="/login">
-					<h4 className={style.login_link}>Вход / Регистрация</h4>
-				</Link>
+				<div className={style.user_logo}></div>
+				<h3>User ???</h3>
 			</div>
+
+			<Link to="/login">
+				<div className={style.login_container}>
+					<div className={style.login}></div>
+					<h4 className={style.login_link}>Вход / Регистрация</h4>
+				</div>
+			</Link>
+
 		</div>
 	)
 }
