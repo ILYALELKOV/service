fbc73db5bac9fc1353c026760ce4cf66d973266f
Ilya Lelkov <ilya.lelkov@mail.ru>
2024-09-21 16:43:13 +1000

    add README.md file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import history from 'connect-history-api-fallback'
 
 export default defineConfig({
 	plugins: [react()],
